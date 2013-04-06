a2d1e596c3d6e8637fa58bb7de17183d4c2de293
loie <github@loie.de>
2013-04-06 15:53:09 +0200

    Added additional demo
    This new demo listens on mouse move. If you move the mouse to the left,
    the image will be moves as if you moved your head to the left, and vice
    versa.

--- js/main.js
+++ js/main.js
@@ -1,34 +1,66 @@
-$(document).ready(function(){
-    $('#eins').click(function(event) {
+/*global $, document */
+$(document).ready(function () {
+    'use strict';
+    $('#one').click(function (event) {
         event.preventDefault();
-        $('#sky').animate({backgroundPosition: '0px 0px'}, 600, 'swing');
-        $('#mountains').animate({backgroundPosition: '0px 0px'}, 600, 'swing');
-        $('#forest').animate({backgroundPosition: '0px 0px'}, 600, 'swing');
-        $('#info').animate({backgroundPosition: '0px 0px'}, 600, 'swing');
+        $('.scroll1').animate({backgroundPosition: '0px 0px'}, 600, 'swing');
+        $('.scroll2').animate({backgroundPosition: '0px 0px'}, 600, 'swing');
     });
 
-    $('#zwei').click(function(event) {
+    $('#two').click(function (event) {
         event.preventDefault();
-        $('#sky').animate({backgroundPosition: '-66px 0px'}, 600, 'swing');
-        $('#mountains').animate({backgroundPosition: '-166px 0px'}, 600, 'swing');
-        $('#forest').animate({backgroundPosition: '-667px 0px'}, 600, 'swing');
-        $('#info').animate({backgroundPosition: '-1667px 0px'}, 600, 'swing');
+        $('.scroll1').animate({backgroundPosition: '-66px 0px'}, 600, 'swing');
+        $('.scroll2').animate({backgroundPosition: '-667px 0px'}, 600, 'swing');
     });
 
-    $('#drei').click(function(event) {
+    $('#three').click(function (event) {
         event.preventDefault();
-        $('#sky').animate({backgroundPosition: '-133px 0px'}, 600, 'swing');
-        $('#mountains').animate({backgroundPosition: '-333px 0px'}, 600, 'swing');
-        $('#forest').animate({backgroundPosition: '-1333px 0px'}, 600, 'swing');
-        $('#info').animate({backgroundPosition: '-3333px 0px'}, 600, 'swing');
+        $('.scroll1').animate({backgroundPosition: '-133px 0px'}, 600, 'swing');
+        $('.scroll2').animate({backgroundPosition: '-1333px 0px'}, 600, 'swing');
     });
 
-    $('#vier').click(function(event) {
+    $('#four').click(function (event) {
         event.preventDefault();
-        $('#sky').animate({backgroundPosition: '-200px 0px'}, 600, 'swing');
-        $('#mountains').animate({backgroundPosition: '-500px 0px'}, 600, 'swing');
-        $('#forest').animate({backgroundPosition: '-2000px 0px'}, 600, 'swing');
-        $('#info').animate({backgroundPosition: '-5000px 0px'}, 600, 'swing');
+        $('.scroll1').animate({backgroundPosition: '-200px 0px'}, 600, 'swing');
+        $('.scroll2').animate({backgroundPosition: '-2000px 0px'}, 600, 'swing');
     });
+
+    $('.table').hover(
+        function mousein(event) {
+            var innerWidth = $('.table').innerWidth();
+            $('.table').mousemove( function (e) {
+                var fraction, movementScroll1, movementScroll2;
+                fraction = e.offsetX / innerWidth;
+                movementScroll1 = (-20 * fraction) + 'px';
+                movementScroll2 = (-40 * fraction) + 'px';
+                $('.scroll1').css({backgroundPosition: movementScroll1 + ' 0px'});
+                $('.scroll2').css({backgroundPosition: movementScroll2 + ' 0px'});
+            });
+        },
+        function mouseout(event) {
+            $('.scroll1').animate({backgroundPosition: '-10px 0px'}, 600, 'swing');
+            $('.scroll2').animate({backgroundPosition: '-20px 0px'}, 600, 'swing');
+        }
+    );
+
+    $('#left').click(function (event) {
+        event.preventDefault();
+        $('.scroll1').animate({backgroundPosition: '0px 0px'}, 600, 'swing');
+        $('.scroll2').animate({backgroundPosition: '0px 0px'}, 600, 'swing');
+    });
+
+    $('#middle').click(function (event) {
+        event.preventDefault();
+        $('.scroll1').animate({backgroundPosition: '-10px 0px'}, 600, 'swing');
+        $('.scroll2').animate({backgroundPosition: '-20px 0px'}, 600, 'swing');
+    });
+
+    $('#right').click(function (event) {
+        event.preventDefault();
+        $('.scroll1').animate({backgroundPosition: '-20px 0px'}, 600, 'swing');
+        $('.scroll2').animate({backgroundPosition: '-40px 0px'}, 600, 'swing');
+    });
+
+   
 });
 
